a12d277dff6590eecb3e01cbc04144855ec61f03
Mrityunjoy Dey <mrityunjoydey1999@gmail.com>
2025-07-01 12:35:57 +0530

    feat(proto): Organize proto files into subdirectories
    - Moves greeter.proto to a subdirectory.
    - Extracts request and response messages into separate files.
    - Updates the script for proto generation to support subdirectories.

--- scripts/generate_proto.sh
+++ scripts/generate_proto.sh
@@ -2,4 +2,5 @@ PROTO_DIR=proto
 MODULE_NAME=github.com/mrityunjoydey/go-grpc
 
 echo "Generating gRPC code..."
-protoc --proto_path="$PROTO_DIR" --go_out=. --go_opt=module="$MODULE_NAME" --go-grpc_out=. --go-grpc_opt=module="$MODULE_NAME" "$PROTO_DIR"/*.proto
+protoc --experimental_allow_proto3_optional --go_out=. --go_opt=module="$MODULE_NAME" --go-grpc_out=. --go-grpc_opt=module="$MODULE_NAME" "$PROTO_DIR"/**/*.proto
+# --experimental_allow_proto3_optional is required for proto3 optional fields
